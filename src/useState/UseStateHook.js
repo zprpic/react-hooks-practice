8207ef11a4e8d6c10c92d0c3a4dcfae9021d0ad5
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-10-11 11:26:26 +0200

    useState hook with input...

--- src/useState/UseStateHook.js
+++ src/useState/UseStateHook.js
@@ -3,15 +3,22 @@ import { useState } from "react";
 
 function UseStateHook() {
   const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
   const handleOnClick = () => {
     setCount(count + 1);
+  };
+
+  const handleOnChange = (e) => {
+    setInputValue(e);
   };
 
   return (
     <div>
       {count}
       <button onClick={handleOnClick}>increment</button>
+      <input type="text" onChange={(e) => handleOnChange(e.target.value)} />
+      <h3>{inputValue}</h3>
     </div>
   );
 }
